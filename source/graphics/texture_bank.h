0bd1faf9de068470c7798b8c2d90a79e8d209102
thomas heegaard <thomas.heegaard@heig-vd.ch>
2014-11-03 17:24:42 +0100

    added all files back into git
    thank god that i didnt wait until i had 400 files to do this...

---
+++ source/graphics/texture_bank.h
@@ -0,0 +1,25 @@
+/*
+=================================== ===== ==== === == = =  =   =    =     =
+|File     : texture_bank.h
+|Author   : Thomas Heegaard
+|Created  : 10/28/2014
+|Purpose  : 
+|
+|Comments : 
+|
+=================================== ===== ==== === == = =  =   =    =     =
+*/
+
+#ifndef TEXTURE_BANK_H
+#define TEXTURE_BANK_H
+
+#define TEXTURE_COUNT   2
+
+#define EXAMPLE1_TEX    0
+#define EXAMPLE2_TEX    1
+
+#define TEXTURE_FILENAMES   \
+    "resources/yoshi.bmp",  \
+    "resources/yoshi.bmp"
+
+#endif //TEXTURE_BANK_H
