0c98594bd4a627f002f603490c06ae7d4868e9a6
thomas heegaard <thomas.heegaard@heig-vd.ch>
2014-11-04 21:42:11 +0100

    refined event handling,
    transfered the SDL layer to the sdl_wrapper file
    still some leftover sdl structures passed to the
    engine though...

---
+++ source/engine/input.c
@@ -0,0 +1,136 @@
+/*
+=================================== ===== ==== === == = =  =   =    =     =
+|File     : input.c
+|Author   : Thomas Heegaard
+|Created  : 11/04/2014
+|Purpose  : 
+|
+|Comments : 
+|
+=================================== ===== ==== === == = =  =   =    =     =
+*/
+
+void InputFocus() 
+{
+    //Pure virtual, do nothing
+}
+
+void InputBlur() 
+{
+    //Pure virtual, do nothing
+}
+
+void KeyDown(SDLKey sym, SDLMod mod, Uint16 unicode) 
+{
+    //Pure virtual, do nothing
+}
+
+void KeyUp(SDLKey sym, SDLMod mod, Uint16 unicode) 
+{
+    //Pure virtual, do nothing
+}
+
+void MouseFocus() 
+{
+    //Pure virtual, do nothing
+}
+
+void MouseBlur() 
+{
+    //Pure virtual, do nothing
+}
+
+void MouseMove(int mX, int mY, int relX, int relY, bool Left,bool Right,bool Middle) 
+{
+    //Pure virtual, do nothing
+}
+
+void MouseWheel(bool Up, bool Down) 
+{
+    //Pure virtual, do nothing
+}
+
+void LButtonDown(int mX, int mY) 
+{
+    //Pure virtual, do nothing
+}
+
+void LButtonUp(int mX, int mY) 
+{
+    //Pure virtual, do nothing
+}
+
+void RButtonDown(int mX, int mY) 
+{
+    //Pure virtual, do nothing
+}
+
+void RButtonUp(int mX, int mY) 
+{
+    //Pure virtual, do nothing
+}
+
+void MButtonDown(int mX, int mY) 
+{
+    //Pure virtual, do nothing
+}
+
+void MButtonUp(int mX, int mY) 
+{
+    //Pure virtual, do nothing
+}
+
+void JoyAxis(Uint8 which,Uint8 axis,Sint16 value) 
+{
+    //Pure virtual, do nothing
+}
+
+void JoyButtonDown(Uint8 which,Uint8 button) 
+{
+    //Pure virtual, do nothing
+}
+
+void JoyButtonUp(Uint8 which,Uint8 button) 
+{
+    //Pure virtual, do nothing
+}
+
+void JoyHat(Uint8 which,Uint8 hat,Uint8 value) 
+{
+    //Pure virtual, do nothing
+}
+
+void JoyBall(Uint8 which,Uint8 ball,Sint16 xrel,Sint16 yrel) 
+{
+    //Pure virtual, do nothing
+}
+
+void Minimize() 
+{
+    //Pure virtual, do nothing
+}
+
+void Restore() 
+{
+    //Pure virtual, do nothing
+}
+
+void Resize(int w,int h) 
+{
+    //Pure virtual, do nothing
+}
+
+void Expose() 
+{
+    //Pure virtual, do nothing
+}
+
+void Exit() 
+{
+    //Pure virtual, do nothing
+}
+
+void User(Uint8 type, int code, void* data1, void* data2) 
+{
+    //Pure virtual, do nothing
+}
--- source/utils/sdl_wrapper.c
+++ source/utils/sdl_wrapper.c
@@ -11,6 +11,7 @@
 */
 
 #include "sdl_wrapper.h"
+#include "../engine/input.h"
 
 
 
@@ -147,6 +148,174 @@ ERR UpdateScreen()
 
     SDL_RenderPresent(screen->renderer);
     return 0;
+}
+
+void HandleEvent(SDL_Event* event) 
+{
+    switch(event->type) 
+    {
+    case SDL_ACTIVEEVENT:
+    {
+        switch(event->active.state) 
+        {
+        case SDL_APPMOUSEFOCUS:
+        {
+            if ( event->active.gain )
+                MouseFocus();
+            else
+                MouseBlur();
+            break;
+        }
+        case SDL_APPINPUTFOCUS:
+        {
+            if ( event->active.gain )
+                InputFocus();
+            else
+                InputBlur();
+            break;
+        }
+        case SDL_APPACTIVE:
+        {
+            if ( event->active.gain )
+                Restore();
+            else
+                Minimize();
+            break;
+        }
+        }
+        break;
+    }
+
+    case SDL_KEYDOWN:
+    {
+        KeyDown(event->key.keysym.sym,event->key.keysym.mod,event->key.keysym.unicode);
+        break;
+    }
+
+    case SDL_KEYUP:
+    {
+        KeyUp(event->key.keysym.sym,event->key.keysym.mod,event->key.keysym.unicode);
+        break;
+    }
+
+    case SDL_MOUSEMOTION:
+    {
+        MouseMove(event->motion.x,event->motion.y,event->motion.xrel,event->motion.yrel,(event->motion.state&SDL_BUTTON(SDL_BUTTON_LEFT))!=0,(event->motion.state&SDL_BUTTON(SDL_BUTTON_RIGHT))!=0,(event->motion.state&SDL_BUTTON(SDL_BUTTON_MIDDLE))!=0);
+        break;
+    }
+
+    case SDL_MOUSEBUTTONDOWN:
+    {
+        switch(event->button.button) 
+        {
+        case SDL_BUTTON_LEFT: 
+        {
+            LButtonDown(event->button.x,event->button.y);
+            break;
+        }
+        case SDL_BUTTON_RIGHT:
+        {
+            RButtonDown(event->button.x,event->button.y);
+            break;
+        }
+        case SDL_BUTTON_MIDDLE:
+        {
+            MButtonDown(event->button.x,event->button.y);
+            break;
+        }
+        }
+        break;
+    }
+
+    case SDL_MOUSEBUTTONUP:    
+    {
+        switch(event->button.button) 
+        {
+        case SDL_BUTTON_LEFT: 
+        {
+            LButtonUp(event->button.x,event->button.y);
+            break;
+        }
+        case SDL_BUTTON_RIGHT:
+        {
+            RButtonUp(event->button.x,event->button.y);
+            break;
+        }
+        case SDL_BUTTON_MIDDLE:
+        {
+            MButtonUp(event->button.x,event->button.y);
+            break;
+        }
+        }
+        break;
+    }
+
+    case SDL_JOYAXISMOTION: 
+    {
+        JoyAxis(event->jaxis.which,event->jaxis.axis,event->jaxis.value);
+        break;
+    }
+
+    case SDL_JOYBALLMOTION: 
+    {
+        JoyBall(event->jball.which,event->jball.ball,event->jball.xrel,event->jball.yrel);
+        break;
+    }
+
+    case SDL_JOYHATMOTION: 
+    {
+        JoyHat(event->jhat.which,event->jhat.hat,event->jhat.value);
+        break;
+    }
+    case SDL_JOYBUTTONDOWN: 
+    {
+        JoyButtonDown(event->jbutton.which,event->jbutton.button);
+        break;
+    }
+
+    case SDL_JOYBUTTONUP: 
+    {
+        JoyButtonUp(event->jbutton.which,event->jbutton.button);
+        break;
+    }
+
+    case SDL_QUIT: 
+    {
+        Exit();
+        break;
+    }
+
+    case SDL_SYSWMEVENT: 
+    {
+        //Ignore
+        break;
+    }
+
+    case SDL_VIDEORESIZE: 
+    {
+        Resize(event->resize.w,event->resize.h);
+        break;
+    }
+
+    case SDL_VIDEOEXPOSE: 
+    {
+        Expose();
+        break;
+    }
+
+    default: 
+    {
+        User(event->user.type,event->user.code,event->user.data1,event->user.data2);
+        break;
+    }
+    }
+}
+
+ERR HandleEvents()
+{
+    SDL_Event event;
+    while(SDL_PollEvent(&event))
+        Handle_Event(&event);
 }
 
 ERR ExitSDL()
