cd2d5657ee6dcfa4d6d418cde89ffa96e5f6794d
thomas heegaard <thomas.heegaard@heig-vd.ch>
2014-11-21 16:53:36 +0100

    Oops, forgot to add iportant files...
    fixed now

---
+++ source/utils/live_define.c
@@ -0,0 +1,44 @@
+//Created by script
+
+#include <string.h>
+#include <stdio.h>
+
+
+int TranslateDefines(const char* value)
+{
+    if(strcmp("ANIMATION", value) == 0)
+        return 1;
+    if(strcmp("PLAY", value) == 0)
+        return 2;
+    if(strcmp("OSCILLATING", value) == 0)
+        return 4;
+    if(strcmp("MULTILOOP", value) == 0)
+        return 8;
+    if(strcmp("FRAMES", value) == 0)
+        return 0;
+    if(strcmp("CURRENT_FRAME", value) == 0)
+        return 1;
+    if(strcmp("DIRECTION", value) == 0)
+        return 2;
+    if(strcmp("TARGET_FPS", value) == 0)
+        return 3;
+    if(strcmp("FRAMES_TO_SKIP", value) == 0)
+        return 4;
+    if(strcmp("LOOPS", value) == 0)
+        return 5;
+    if(strcmp("CURRENT_LOOP", value) == 0)
+        return 6;
+    if(strcmp("TEXTURE_COUNT", value) == 0)
+        return 4;
+    if(strcmp("EXAMPLE1_TEX", value) == 0)
+        return 0;
+    if(strcmp("EXAMPLE2_TEX", value) == 0)
+        return 1;
+    if(strcmp("SHIP1_TEX", value) == 0)
+        return 2;
+    if(strcmp("FIRE_TEX", value) == 0)
+        return 3;
+
+    fprintf(stderr, "No define found for %s returning 0\n", value);
+    return 0;
+}
---
+++ source/utils/live_define.h
@@ -0,0 +1,18 @@
+/*
+=================================== ===== ==== === == = =  =   =    =     =
+|File     : live_define.h
+|Author   : Thomas Heegaard
+|Created  : 11/11/2014
+|Purpose  : 
+|
+|Comments : 
+|
+=================================== ===== ==== === == = =  =   =    =     =
+*/
+
+#ifndef LIVE_DEFINE_H
+#define LIVE_DEFINE_H
+
+int TranslateDefines(const char* value);
+
+#endif //LIVE_DEFINE_H
